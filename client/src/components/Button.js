8917103a87e253139e49224894f62af1261c5c82
TImmy <ayantunjitimilehin@yahoo.com>
2021-06-26 18:52:26 -0700

    updated button component based on variant

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,34 +1,34 @@
 import React from "react";
 import { Button as MuiButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   button: {
-    backgroundColor: (props) =>
-      props.colorVariant === "primary" ? theme.palette.primary.main : "#FFFFFF",
-    color: (props) =>
-      props.colorVariant !== "primary" ? theme.palette.primary.main : "#FFFFFF",
-    boxShadow: (props) =>
-      props.colorVariant !== "primary"
-        ? "0px 0px 4px 4px rgba(0, 0, 0, 0.1)"
-        : "",
-    padding: "0.8rem 3rem",
+    padding: theme.spacing(1.6, 6),
     fontSize: 18,
-    transition: `all .7s cubic-bezier(.2,1,.22,1)`,
+    transition: "all .7s cubic-bezier(.2,1,.22,1)",
     "&:hover": {
-      transform: `translateY(-1.7px)`,
-      backgroundColor: (props) =>
-        props.colorVariant === "primary"
-          ? theme.palette.primary.main
-          : "#FFFFFF",
-      color: (props) =>
-        props.colorVariant !== "primary"
-          ? theme.palette.primary.main
-          : "#FFFFFF",
+      transform: "translateY(-1.7px)",
     },
     [theme.breakpoints.down("xs")]: {
-      padding: "0.5rem 2rem",
+      padding: theme.spacing(1, 4),
       fontSize: 16,
+    },
+  },
+  primary: {
+    backgroundColor: theme.palette.primary.main,
+    color: "#FFFFFF",
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+  secondary: {
+    backgroundColor: "#FFFFFF",
+    color: theme.palette.primary.main,
+    boxShadow: "0px 0px 4px 4px rgba(0, 0, 0, 0.1)",
+    "&:hover": {
+      backgroundColor: "#FFFFFF",
     },
   },
 }));
@@ -37,8 +37,14 @@ const Button = (props) => {
   const classes = useStyles(props);
   const { onClick, children, type } = props;
 
+  const btnClasses = clsx({
+    [classes.button] : true,
+    [classes.primary] : props.colorVariant === "primary",
+    [classes.secondary] : props.colorVariant !== "primary"
+})
+
   return (
-    <MuiButton className={classes.button} onClick={onClick} type={type}>
+    <MuiButton className={btnClasses} onClick={onClick} type={type}>
       {children}
     </MuiButton>
   );
